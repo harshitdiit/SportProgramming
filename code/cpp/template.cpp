d6a63a90acf264349c4b6f6b6867d27993047733
Harshit Dhawan <harshitdiit@gmail.com>
2020-11-17 17:51:59 +0530

    Added new contest

---
+++ code/cpp/template.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+
+// File I/O
+#define finput ifstream cin;cin.open("in.txt")
+#define foutput ofstream cout;cout.open("out.txt")
+
+// Fast cin-cout
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+using namespace std;
+
